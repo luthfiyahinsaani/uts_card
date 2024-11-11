96326c83295ed32ec728b9d87f61f63452efe77f
luthfiyahinsaani <luthfiyahinsaani123@gmail.com>
2024-11-11 21:42:28 +0700

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontfamily: {
+      afacad: ['afacad flux', 'sans-serif'],
+      strike: ['protest strike', 'sans-serif']
+    }
+  },
+  plugins: [],
+}
